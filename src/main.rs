a3e3fffc80c591c6e0011ead9cb1380141e4cdb6
Cristian Tentella <cristiantentella@tuta.com>
2025-03-24 13:51:55 +0100

    Add repeated jpeg compression functionality

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-use clap::Parser;
+use std::io::Cursor;
 
-#[derive(clap::Parser, Debug)]
+use clap::Parser;
+use image::{codecs::jpeg::JpegEncoder, ImageReader};
+
+#[derive(Parser, Debug)]
 struct CommandLineArguments {
     input_path: String,
     output_path: String,
     #[arg(short, long, default_value_t = 1)]
     repetitions: u8,
-    #[arg(short, long, default_value_t = 75)]
+    #[arg(short, long, default_value_t = 1)]
     quality: u8,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let command_line_arguments = CommandLineArguments::parse();
+    
+    let mut image = ImageReader::open(&command_line_arguments.input_path)?.decode()?;    
+    
+    for i in 1..=command_line_arguments.repetitions {
+        println!("Repetition {}...", i);
+        
+        let mut writer = Cursor::new(Vec::<u8>::new());
+        let mut jpeg_encoder = JpegEncoder::new_with_quality(&mut writer, command_line_arguments.quality);
+        jpeg_encoder.encode_image(&image)?;
+
+        writer.set_position(0);
+        image = image::load(&mut writer, image::ImageFormat::Jpeg)?;
+    }
+    
+    image.save(&command_line_arguments.output_path)?;
+
+    Ok(())
 }
