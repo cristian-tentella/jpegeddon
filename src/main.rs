75c674522b1bd5cfb51af8d9e121f4cf6da098f3
Cristian Tentella <cristiantentella@tuta.com>
2025-03-24 12:54:08 +0100

    Add command line arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+struct CommandLineArguments {
+    input_path: String,
+    output_path: String,
+    #[arg(short, long, default_value_t = 1)]
+    repetitions: u8,
+    #[arg(short, long, default_value_t = 75)]
+    quality: u8,
+}
+
+fn main() {
+    let command_line_arguments = CommandLineArguments::parse();
+}
